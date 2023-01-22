28fd42d1ee2c1dcf006f6399c84e7a703d3fbf96
mjuna <mjuna@LAPTOP-I83S08OS>
2023-01-22 14:52:44 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.WebSockets;
+using CosmosGraph;
+using Gremlin.Net.Driver;
+using Gremlin.Net.Driver.Exceptions;
+using Gremlin.Net.Structure.IO.GraphSON;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+
+var configuration = new ConfigurationBuilder()
+     .AddJsonFile($"appsettings.json")
+     .AddEnvironmentVariables()
+     .Build();
+
+string Host = configuration["Host"];
+string PrimaryKey = configuration["PrimaryKey"];
+string Database = configuration["Database"];
+string Container = configuration["Container"];
+int.TryParse(configuration["Port"], out int Port);
+bool.TryParse(configuration["EnableSSL"], out bool EnableSSL);
+string containerLink = $"/dbs/{Database}/colls/{Container}";
+
+var connectionString = configuration["ConnectionString"];
+
+var gremlinServer = new GremlinServer(Host, Port, enableSsl: EnableSSL,
+                                                   username: containerLink,
+                                                   password: PrimaryKey);
+ConnectionPoolSettings connectionPoolSettings = new ConnectionPoolSettings()
+{
+    MaxInProcessPerConnection = 10,
+    PoolSize = 30,
+    ReconnectionAttempts = 3,
+    ReconnectionBaseDelay = TimeSpan.FromMilliseconds(500)
+};
+
+var webSocketConfiguration =
+    new Action<ClientWebSocketOptions>(options =>
+    {
+        options.KeepAliveInterval = TimeSpan.FromSeconds(10);
+    });
+
+using (var gremlinClient = new GremlinClient(
+    gremlinServer,
+    new GraphSON2Reader(),
+    new GraphSON2Writer(),
+    "application/vnd.gremlin-v2.0+json",
+    connectionPoolSettings,
+    webSocketConfiguration))
+{
+    RunQuery(gremlinClient, Seed.Initialise());
+    RunQuery(gremlinClient, Seed.AddPerson());
+    RunQuery(gremlinClient, Seed.AddGroup());
+    RunQuery(gremlinClient, Seed.AddCourses());
+    RunQuery(gremlinClient, Seed.EnrolPersonToCourses());
+    RunQuery(gremlinClient, Seed.AssignPersonToGroups());
+}
+
+void RunQuery(GremlinClient gremlinClient, IList<string> query)
+{
+    // Create async task to execute the Gremlin query.
+
+    foreach (var q in query)
+    {
+        Console.WriteLine(String.Format("Running this query: {0}", q));
+        var resultSet = SubmitRequest(gremlinClient, q).Result;
+        if (resultSet.Count > 0)
+        {
+            Console.WriteLine("\tResult:");
+            foreach (var result in resultSet)
+            {
+                // The vertex results are formed as Dictionaries with a nested dictionary for their properties
+                string output = JsonConvert.SerializeObject(result);
+                Console.WriteLine($"\t{output}");
+            }
+            Console.WriteLine();
+        } 
+    }
+
+}
+
+Task<ResultSet<dynamic>> SubmitRequest(GremlinClient gremlinClient, string query)
+{
+    try
+    {
+        return gremlinClient.SubmitAsync<dynamic>(query);
+    }
+    catch (ResponseException e)
+    {
+        // Print the Gremlin status code.
+        Console.WriteLine($"\tError StatusCode: {e.StatusCode}");
+
+        throw;
+    }
+}
---
+++ Seed.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmosGraph
+{
+    public static class Seed
+    {
+        public static IList<string> Initialise()
+        {
+            return new List<string>
+            { 
+                "g.V().drop()"
+            };
+        }
+
+        public static IList<string> AddPerson()
+        {
+            return new List<string>
+            {
+                "g.addV('person').property('id', 'thomas').property('firstName', 'Thomas').property('lastName', 'T').property('email', 'Thomas.T@gmail.com').property('JobTitle', 'Software Engineer').property('tenant', 'test')",
+                "g.addV('person').property('id', 'mary').property('firstName', 'Mary').property('lastName', 'M').property('email', 'Mary.M@outlook.com').property('JobTitle', 'Auditor').property('tenant', 'test')",
+                "g.addV('person').property('id', 'john').property('firstName', 'John').property('lastName', 'J').property('email', 'John.J@outlook.com').property('JobTitle', 'DevOps Engineer').property('tenant', 'test')",
+                "g.addV('person').property('id', 'smith').property('firstName', 'Smith').property('lastName', 'S').property('email', 'Smith.S@outlook.com').property('JobTitle', 'DevOps Engineer').property('tenant', 'test')",
+                "g.addV('person').property('id', 'ben').property('firstName', 'Ben').property('lastName', 'B').property('email', 'Ben.B@outlook.com').property('JobTitle', 'Auditor').property('tenant', 'test')",
+                "g.addV('person').property('id', 'daisy').property('firstName', 'Daisy').property('lastName', 'D').property('email', 'Daisy.D@outlook.com').property('JobTitle', 'Software Engineer').property('tenant', 'test')",
+            };
+        }
+
+        public static IList<string> AddGroup()
+        {
+            return new List<string>
+            {
+                "g.addV('group').property('id', 'director').property('title', 'Director').property('Grade', 'A').property('experience', '10').property('tenant', 'test')",
+                "g.addV('group').property('id', 'seniorManager').property('title', 'Senior Manager').property('Grade', 'B').property('experience', '8').property('tenant', 'test')",
+                "g.addV('group').property('id', 'manager').property('title', 'Manager').property('Grade', 'C').property('experience', '5').property('tenant', 'test')",
+                "g.addV('group').property('id', 'assistantManager').property('title', 'Assistant Manager').property('Grade', 'D').property('experience', '3').property('tenant', 'test')",
+            };
+        }
+
+        public static IList<string> AddCourses()
+        {
+            return new List<string>
+            {
+                $"g.addV('course').property('id', 'itw').property('title', 'Introduction to Word').property('Summary', 'Summary for Introduction to Word').property('publishedDate', '{DateTime.UtcNow.AddDays(-1)}').property('price', '10').property('tenant', 'test')",
+                $"g.addV('course').property('id', 'efe').property('title', 'Excel for experts').property('Summary', 'Summary for Excel for experts').property('publishedDate', '{DateTime.UtcNow.AddDays(-2)}').property('price', '40').property('tenant', 'test')",
+                $"g.addV('course').property('id', 'cc').property('title', 'Cloud Computing').property('Summary', 'Summary for Cloud Computing').property('publishedDate', '{DateTime.UtcNow.AddDays(-3)}').property('price', '50').property('tenant', 'test')",
+                $"g.addV('course').property('id', 'itc').property('title', 'Introduction to C#').property('Summary', 'Summary for Introduction to C#').property('publishedDate', '{DateTime.UtcNow.AddDays(-4)}').property('price', '60').property('tenant', 'test')",
+                $"g.addV('course').property('id', 'jfd').property('title', 'Java for dummies').property('Summary', 'Summary for Java for dummies').property('publishedDate', '{DateTime.UtcNow.AddDays(-5)}').property('price', '55').property('tenant', 'test')",
+                $"g.addV('course').property('id', 'top').property('title', 'Theory of programming').property('Summary', 'Summary for Theory of programming').property('publishedDate', '{DateTime.UtcNow.AddDays(-6)}').property('price', '20').property('tenant', 'test')",
+                $"g.addV('course').property('id', 'jur').property('title', 'Javascript using React').property('Summary', 'Summary for Javascript using React').property('publishedDate', '{DateTime.UtcNow.AddDays(-7)}').property('price', '40').property('tenant', 'test')",
+            };
+        }
+
+        public static IList<string> AssignPersonToGroups()
+        {
+            return new List<string>
+            {
+                "g.V('director').addE('enrolled').to(g.V('ben'))",
+                "g.V('seniorManager').addE('enrolled').to(g.V('mary'))",
+                "g.V('manager').addE('enrolled').to(g.V('thomas'))",
+                "g.V('manager').addE('enrolled').to(g.V('daisy'))",
+                "g.V('assistantManager').addE('enrolled').to(g.V('smith'))",
+                "g.V('assistantManager').addE('enrolled').to(g.V('john'))",
+            };
+        }
+
+        public static IList<string> EnrolPersonToCourses()
+        {
+            return new List<string>
+            {
+                "g.V('thomas').addE('member').to(g.V('itc'))",
+                "g.V('thomas').addE('enrolled').to(g.V('jfd'))",
+                "g.V('thomas').addE('enrolled').to(g.V('top'))",
+                "g.V('mary').addE('enrolled').to(g.V('itw'))",
+                "g.V('mary').addE('enrolled').to(g.V('efe'))",
+                "g.V('john').addE('enrolled').to(g.V('cc'))",
+                "g.V('smith').addE('enrolled').to(g.V('cc'))",
+                "g.V('ben').addE('enrolled').to(g.V('top'))",
+                "g.V('daisy').addE('enrolled').to(g.V('efe'))",
+            };
+        }
+
+    }
+}
